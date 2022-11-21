3e33b12581c0db61a9f3b05dd4b35ac2052e4242
coralsprings-tcs <115885261+coralsprings-tcs@users.noreply.github.com>
2022-11-21 10:51:47 -0500

    changes

--- PETS/original/scenes/UserMenu.gd
+++ PETS/original/scenes/UserMenu.gd
@@ -6,17 +6,21 @@ onready var BarbecueFish = $feed/BarbecueFish
 func _ready():
 	# set invisible
 	MustardHam.visible = false
+	BarbecueFish.visible = false
 
 func _on_FeedButton_pressed():
-	$feed/BarbecueFish.visible = false
 	if $feed/MustardHam.visible == false:
 		$feed/MustardHam.visible = true
 		var MustardTimer = get_tree().create_timer(2)
-		print('feed should be invisible')
+		print('Mustard Ham')
 		yield(MustardTimer, "timeout")
 		$feed/MustardHam.visible = false
-
-		
+	if $feed/BarbecueFish.visible == false:
+		$feed/BarbecueFish.visible = true
+		var BarbecueTimer = get_tree().create_timer(3)
+		print('Barbecue Fish')
+		yield(BarbecueTimer, "timeout")
+		$feed/BarbecueFish.visible = false
 
 		
 	#TODO:
--- PETS/scenes/UserMenu.gd
+++ PETS/scenes/UserMenu.gd
@@ -1,19 +1,38 @@
 extends Control
 
 onready var MustardHam = $feed/MustardHam
+onready var BarbecueFish = $feed/BarbecueFish
+onready var RelishLobster = $feed/RelishLobster
 
 func _ready():
 	$feed/MustardHam.visible = false
+	$feed/BarbecueFish.visible = false
+	$feed/RelishLobster.visible = false
 
-func _on_FeedButton_pressed():
+func _on_MustardHamButton_pressed():
 	if $feed/MustardHam.visible == false:
 		$feed/MustardHam.visible = true
-		var timer = get_tree().create_timer(2)
-		print('feed should be invisible')
-		yield(timer, "timeout")
+		var MustardHamTimer = get_tree().create_timer(2)
+		print('MUSTARD HAM')
+		yield(MustardHamTimer, "timeout")
 		$feed/MustardHam.visible = false
 		
-	
+func _on_BarbecueFishButton_pressed():
+	if $feed/BarbecueFish.visible == false:
+		$feed/BarbecueFish.visible = true
+		var BarbecueFishTimer = get_tree().create_timer(3)
+		print('BARBECUE FISH')
+		yield(BarbecueFishTimer, "timeout")
+		$feed/BarbecueFish.visible = false
+		
+func _on_RelishLobsterButton_pressed():
+	if $feed/RelishLobster.visible == false:
+		$feed/RelishLobster.visible = true
+		var RelishLobsterTimer = get_tree().create_timer(4)
+		print('RELISH LOBSTER')
+		yield(RelishLobsterTimer, "timeout")
+		$feed/RelishLobster.visible = false
+
 	#TODO:
 	# - make it so hunger decreases every time you press feed button
 	# - do not worry at this point about negative numbers or greater than 100 for percent

23a4175215883224985b1b9ce85a39cf1e1ff2e6
coralsprings-tcs <115885261+coralsprings-tcs@users.noreply.github.com>
2022-12-03 10:56:17 -0500

    made changes

--- PETS/scenes/UserMenu.gd
+++ PETS/scenes/UserMenu.gd
@@ -3,13 +3,18 @@ extends Control
 onready var MustardHam = $feed/MustardHam
 onready var BarbecueFish = $feed/BarbecueFish
 onready var RelishLobster = $feed/RelishLobster
+onready var MayoShrimp = $feed/MayoShrimp
 
 func _ready():
 	$feed/MustardHam.visible = false
 	$feed/BarbecueFish.visible = false
 	$feed/RelishLobster.visible = false
+	$feed/MayoShrimp.visible = false
 
 func _on_MustardHamButton_pressed():
+	$feed/BarbecueFish.visible = false
+	$feed/RelishLobster.visible = false
+	$feed/MayoShrimp.visible = false
 	if $feed/MustardHam.visible == false:
 		$feed/MustardHam.visible = true
 		var MustardHamTimer = get_tree().create_timer(2)
@@ -18,6 +23,9 @@ func _on_MustardHamButton_pressed():
 		$feed/MustardHam.visible = false
 		
 func _on_BarbecueFishButton_pressed():
+	$feed/MustardHam.visible = false
+	$feed/RelishLobster.visible = false
+	$feed/MayoShrimp.visible = false
 	if $feed/BarbecueFish.visible == false:
 		$feed/BarbecueFish.visible = true
 		var BarbecueFishTimer = get_tree().create_timer(3)
@@ -26,13 +34,28 @@ func _on_BarbecueFishButton_pressed():
 		$feed/BarbecueFish.visible = false
 		
 func _on_RelishLobsterButton_pressed():
+	$feed/MustardHam.visible = false
+	$feed/BarbecueFish.visible = false
+	$feed/MayoShrimp.visible = false
 	if $feed/RelishLobster.visible == false:
 		$feed/RelishLobster.visible = true
 		var RelishLobsterTimer = get_tree().create_timer(4)
 		print('RELISH LOBSTER')
 		yield(RelishLobsterTimer, "timeout")
 		$feed/RelishLobster.visible = false
-
+		
+func _on_MayoShrimpButton_pressed():
+	$feed/MustardHam.visible = false
+	$feed/BarbecueFish.visible = false
+	$feed/RelishLobster.visible = false
+	if $feed/MayoShrimp.visible == false:
+		$feed/MayoShrimp.visible = true
+		var MayoShrimpTimer = get_tree().create_timer(5)
+		print('MAYO SHRIMP')
+		yield(MayoShrimpTimer, "timeout")
+		$feed/MayoShrimp.visible = false
+	
 	#TODO:
 	# - make it so hunger decreases every time you press feed button
 	# - do not worry at this point about negative numbers or greater than 100 for percent
+
